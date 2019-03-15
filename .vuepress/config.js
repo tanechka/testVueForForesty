6c08ff54087f2a6c0f14f2ee517cb0caadde3ddf
Radyuk Tatyana <t.radyuk@itransition.com>
2019-03-15 18:28:51 +0300

    test

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Outreach Shame',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Blog', link: '/blog/' }
+        ],
+        sidebar: [
+            '/',
+            '/blog/'
+        ]
+    }
+}
